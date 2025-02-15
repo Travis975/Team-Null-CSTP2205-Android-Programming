896a14ffb9c475327c26cb542f66742d363dc9cb
Travis Schellenberg <travis.schellenberg@gmail.com>
2025-02-14 17:43:13 -0800

    Updated code to get first 2 screens working

--- app/src/main/java/com/example/overrun/navigation/Routes.kt
+++ app/src/main/java/com/example/overrun/navigation/Routes.kt
@@ -1,17 +1,17 @@
 package com.example.overrun.navigation
 
 import androidx.compose.runtime.Composable
-import androidx.navigation.compose.Routes
+import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.Overrun.ui.screens.DetailsScreen
-import com.example.Overrun.ui.screens.HomeScreen
+import com.example.overrun.ui.screens.DetailsScreen
+import com.example.overrun.ui.screens.HomeScreen
 
 @Composable
 fun AppRoutes() {
     val navController = rememberNavController()
 
-    NavRoutes(navController = navController, startDestination = "home") {
+    NavHost(navController = navController, startDestination = "home") {
         composable("home") { HomeScreen(navController) }
         composable("details") { DetailsScreen(navController) }
     }
--- app/src/main/java/com/example/overrun/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/overrun/ui/screens/HomeScreen.kt
@@ -8,7 +8,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 
 @Composable
 fun HomeScreen(navController: NavController) {
@@ -22,4 +24,12 @@ fun HomeScreen(navController: NavController) {
             Text("Go to Details")
         }
     }
+}
+
+
+// Preview without NavController
+@Preview(showBackground = true)
+@Composable
+fun HomeScreenPreview() {
+    HomeScreen(navController = rememberNavController())
 }
