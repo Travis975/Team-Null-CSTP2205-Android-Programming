fdd877fb35826606e50e346fdc39808e5097e2c9
Samuel_Chow <demondemontcchow@gmail.com>
2025-02-15 13:50:23 -0800

    (1) Add Game Constant kt file (DefineAppConstant.kt) (2) Add Route Constant Enum mapping (3) Refactor the existing route with the Route Enum path

---
+++ app/src/main/java/com/example/overrun/enitities/DefineAppConstant.kt
@@ -0,0 +1,10 @@
+package com.example.overrun.enitities
+
+enum class Route(val path : String){
+    HOME("home"),
+    DETAILS("details"),
+    MAIN_MENU("mainMenu"),
+    LEVEL_1("level1"),
+    TEST_CHARACTER("TestCharacter")
+
+}
--- app/src/main/java/com/example/overrun/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/overrun/ui/screens/HomeScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.example.overrun.enitities.Route.*
 
 @Composable
 fun HomeScreen(navController: NavController) {
@@ -20,7 +21,7 @@ fun HomeScreen(navController: NavController) {
         verticalArrangement = Arrangement.Center
     ) {
         Text(text = "Home Screen")
-        Button(onClick = { navController.navigate("details") }) {
+        Button(onClick = { navController.navigate(DETAILS.path) }) {
             Text("Go to Details")
         }
     }
