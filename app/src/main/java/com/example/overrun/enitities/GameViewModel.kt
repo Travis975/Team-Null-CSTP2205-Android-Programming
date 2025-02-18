65ab0a938b73c609b940cc740ae9fb1d971320e8
Samuel_Chow <demondemontcchow@gmail.com>
2025-02-18 00:19:45 -0800

    (1) Add GameViewModel (2) Add enum hero type (3) Modify default hero initiate pos to be 0,0 (4) Add hero type setup in HeroCharacter Object (5) Move hero object creation in GameViewModel (6) Change TestCharacterScreen use GameViewModel hero obj

---
+++ app/src/main/java/com/example/overrun/enitities/GameViewModel.kt
@@ -0,0 +1,15 @@
+package com.example.overrun.enitities
+
+import androidx.lifecycle.ViewModel
+import com.example.gohero.enitities.GameConstant.HERO_PIXEL_SIZE
+import com.example.gohero.enitities.character.HeroCharacter
+import com.example.overrun.R
+import com.example.overrun.enitities.sprites.loadSpriteSheet
+
+class GameViewModel : ViewModel(){
+
+    // Hero data object
+    val hero = HeroCharacter()
+
+
+}
--- app/src/main/java/com/example/overrun/enitities/character/HeroCharacter.kt
+++ app/src/main/java/com/example/overrun/enitities/character/HeroCharacter.kt
@@ -3,9 +3,16 @@ package com.example.gohero.enitities.character
 import com.example.gohero.enitities.GameConstant.DEFAULT_HERO_SPEED
 import com.example.gohero.enitities.GameConstant.DEFAULT_LIVES
 import com.example.gohero.enitities.eCharactertype
+import com.example.gohero.enitities.eHerotype
+import com.example.gohero.enitities.eHerotype.*
 
-class HeroCharacter(pairStartPos : Pair<UInt, UInt>) :
-    CharacterBase(eCharactertype.eHERO, pairStartPos, DEFAULT_LIVES, DEFAULT_HERO_SPEED)
+class HeroCharacter() :
+    CharacterBase(eCharactertype.eHERO, DEFAULT_LIVES, DEFAULT_HERO_SPEED)
 {
+    private var eType : eHerotype = eHerotype.eHERO_TOKAGE
 
+    public fun setHeroType(eType : eHerotype){
+        this.eType = eType
+    }
+    public fun getHeroType() = eType
 }
