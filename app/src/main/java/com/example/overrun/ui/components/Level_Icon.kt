232938ed623261430919f7e22a6abe3c503c40a2
Travis Schellenberg <travis.schellenberg@gmail.com>
2025-04-06 13:05:55 -0700

    Just added level 3 icon

--- app/src/main/java/com/example/overrun/ui/components/Level_Icon.kt
+++ app/src/main/java/com/example/overrun/ui/components/Level_Icon.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -18,7 +17,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -47,8 +45,7 @@ fun LevelIcon(level: Int, isSelected: Boolean, onClick: () -> Unit) {
                     id = when (level) {
                         1 -> R.drawable.level_1_icon
                         2 -> R.drawable.level_2_icon
-                        // Commenting out Level 3 for now
-                        // 3 -> R.drawable.level_3_icon
+                        3 -> R.drawable.level_3_icon
                         else -> R.drawable.unavailable_icon
                     }
                 ),
